d7627e66f16d63fde6c0f483191c870a341568be
titi-lima <titisau@gmail.com>
2023-09-11 21:02:52 -0300

    feat: topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Github } from "lucide-react";
 import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
 
-function App() {
-  return <Button>Hello World!</Button>;
+export function App() {
+  return (
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 🩵 no NLW da Rocketseat
+          </span>
+          <Separator className="h-6" orientation="vertical"></Separator>
+          <Button variant="outline">
+            <Github size={16} className="m-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default App;
